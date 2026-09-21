4396627c260cd48eeea7d0c29c53290129411ebb
PIOWYU <piowyu45682@gmail.com>
2026-09-21 20:37:46 +0800

    Add bonus knife illustration to rogue cards

--- src/data/cardArt.ts
+++ src/data/cardArt.ts
@@ -1,10 +1,12 @@
 import throwImage from '../../assets/cards/rogue/Throw.png';
 import shadowImage from '../../assets/cards/rogue/Shadow.png';
 import whirlImage from '../../assets/cards/rogue/Whirl.png';
+import knifeImage from '../../assets/cards/rogue/Knife.png';
 import type { CardId } from './cards';
 
 export const cardArt: Partial<Record<CardId, string>> = {
   throw: throwImage,
   shadow: shadowImage,
-  whirl: whirlImage
+  whirl: whirlImage,
+  knife: knifeImage
 };
